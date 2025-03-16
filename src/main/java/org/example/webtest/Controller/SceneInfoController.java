bee295dc42cca37cebbfe54000e3d18c2ddad917
mvxi <mvxi@163.com>
2025-03-16 22:36:15 +0800

    modify scene type

--- src/main/java/org/example/webtest/Controller/SceneInfoController.java
+++ src/main/java/org/example/webtest/Controller/SceneInfoController.java
@@ -8,11 +8,11 @@ import java.util.HashMap;
 @RestController
 public class SceneInfoController {
     final HashMap<String, String> sceneType = new HashMap<String, String>()  {{
-        put("0", "安全生产");
-        put("1", "交通管理");
-        put("2", "市容市政");
-        put("3", "自然灾害");
-        put("4", "水文气象");
+        put("10000", "安全生产");
+        put("10100", "交通管理");
+        put("10200", "市容市政");
+        put("10300", "自然灾害");
+        put("10400", "水文气象");
     }};
     @GetMapping("/scene_type")
     public APIResponsePacker<HashMap<String, String>> getSceneType() {
